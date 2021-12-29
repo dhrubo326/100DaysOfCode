742d007312309c26548622749fb85c1d0d5b91a9
sakhaought dhrubo <dhrubo326@gmail.com>
2021-12-29 12:21:49 +0600

    function basic

---
+++ day_7/highest_even.py
@@ -0,0 +1,9 @@
+def highest_even(list):
+  highest = 0
+  for li_item in list:
+    if li_item % 2 == 0:
+      if li_item > highest:
+        highest = li_item
+  return highest
+
+print(highest_even([12, 2, 4, 5, 6, 11, 10]))
